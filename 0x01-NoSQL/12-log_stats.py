ba4b38de986a10053c72f300b9ca40d2bc1c917f
sanmi <sossesam@gmail.com>
2024-10-22 20:00:46 +0100

    all

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" 12-main """
+import bson
+
+
+with open('dump/logs/nginx.bson', 'rb') as f:
+    data = bson.decode_all(f.read())
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    count = len(data)
+    get = len([x for x in data if x['method'] == "GET"])
+    status = len([x for x in data if x['path'] == "/status"])
+
+    method_count = {}
+    for met in method:
+        method_count[met] = len([x for x in data if x['method'] == met])
+
+    print(f"{count} logs")
+    print("Methods:")
+
+    for me in method_count.keys():
+        print(f"  method {me}: {method_count[me]}")
+    print(f"{status} status check")
