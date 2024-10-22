8b4513f6d76d98b96cb3fe08ab07cb819699c06e
sanmi <sossesam@gmail.com>
2024-10-22 11:20:14 +0100

    all

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" 8-main """
+
+import pymongo
+
+def list_all(mongo_collection):
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = myclient["my_db"]
+    table = db['school']
+
+    return table.find()
