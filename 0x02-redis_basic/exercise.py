d7512fef20e48d900c0a16129195cc1753be7199
sanmi <sossesam@gmail.com>
2024-10-25 05:26:58 +0100

    all

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""0x02. Redis basic"""
 import redis
 import uuid
 
+
 class Cache:
+    """0x02. Redis basic"""
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
