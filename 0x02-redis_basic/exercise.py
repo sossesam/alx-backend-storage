a6171eb9e8086d990b35fe19e994ff84806848f8
sanmi <sossesam@gmail.com>
2024-10-25 08:36:15 +0100

    all

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
-"""0x02. Redis basic"""
 import redis
 import uuid
 
-
 class Cache:
-    """0x02. Redis basic"""
     def __init__(self):
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(host="localhost", port=6379, db=0)
         self._redis.flushdb()
 
     def store(self, data):
         id = str(uuid.uuid4())
-        self._redis.mset({id:data})
+        self._redis.set(id, data)
         return id
+    
+    def get(self, key, fn=None):
+        data = self._redis.get(key)
+
+        if data is None:
+            return data
+        
+        if fn:
+            new_data = fn(data)
+            return new_data
+        else:
+            return data
+        
+    def get_str(self, key):
+        return self.get(key, lambda d: d.decode('utf-8'))
+    
+    def get_int(self, key):
+        return self.get(key, lambda d: int(d))
