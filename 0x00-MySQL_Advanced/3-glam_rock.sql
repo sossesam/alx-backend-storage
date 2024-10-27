47fa5930dd404b043617e7142648bd0523a1d257
sanmi <sossesam@gmail.com>
2024-10-27 02:53:26 +0100

    all

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- Write a SQL script that creates a table users following these requirements:
 
-SELECT band_name, (IFNULL(split, 2022) - formed)  AS lifespan FROM metal_bands ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
